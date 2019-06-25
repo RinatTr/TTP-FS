d108bca1a7123eb7546a6dfa3455c87f38042ed8
RinatTr <rinatreg@gmail.com>
2019-06-25 16:23:56 -0400

    init db

---
+++ backend/db/seed.sql
@@ -0,0 +1,22 @@
+DROP DATABASE IF EXISTS stockify;
+CREATE DATABASE stockify;
+
+\c stockify;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR UNIQUE NOT NULL,
+  password_digest VARCHAR UNIQUE NOT NULL,
+  email VARCHAR UNIQUE NOT NULL,
+  balance DECIMAL (18, 2) DEFAULT 5000.00
+);
+
+CREATE TABLE transactions (
+  id SERIAL PRIMARY KEY,
+  user_id INT REFERENCES users(id),
+  ticker_symbol VARCHAR,
+  shares INT,
+  time_stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  sale_price VARCHAR,
+  type VARCHAR
+);
