0bbf47b11aa7bcafbf1331930b5b677db9855b92
RinatTr <rinatreg@gmail.com>
2019-06-25 22:10:31 -0400

    add root and auth reducers

---
+++ frontend/src/reducers/AuthReducer.js
@@ -0,0 +1,17 @@
+// import { SIGN_UP, LOGIN, RECEIVE_ERROR } from '../actions/AuthActions.js';
+
+const AuthReducer = (oldState = {}, action) => {
+  Object.freeze(oldState);
+  switch (action.type) {
+    case SIGN_UP:
+      return { signedUpUser: action.signedUpUser}
+    case LOGIN:
+      return { loggedUser: action.loggedUser}
+    case RECEIVE_ERROR:
+      return { error: action.error}
+    default:
+      return oldState
+  }
+}
+
+export default AuthReducer;
