6544d436804be81ddfb6ed49d93048fc30b685f6
RinatTr <rinatreg@gmail.com>
2019-06-26 15:58:00 -0400

    init portfolio FE route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
 import './App.css';
 
 import AuthContainer from './components/Auth/AuthContainer.js'
 import TransactionsContainer from './components/Transactions/TransactionsContainer.js'
+import Portfolio from './components/Portfolio/Portfolio.js'
 
 
 function App() {
   return (
     <div className="App">
+      <nav>
+        <Link to="/transactions">transactions</Link>
+        <Link to="/portfolio">portfolio</Link>
+      </nav>
       <Switch>
         <Route path="/auth/login" component={AuthContainer} />
         <Route path="/auth/signup" component={AuthContainer} />
         <Route path="/transactions" component={TransactionsContainer} />
+        <Route path="/portfolio" component={Portfolio} />
       </Switch>
     </div>
   );
---
+++ frontend/src/components/Portfolio/Portfolio.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Portfolio extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        "portfolio"
+      </React.Fragment>
+    )
+  }
+}
