2c0cdeb842e388b4d27e7e3a407db302fa8ca640
RinatTr <rinatreg@gmail.com>
2019-06-25 23:31:34 -0400

    add auth routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+
+import AuthContainer from './components/Auth/AuthContainer.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Switch>
+        <Route path="/auth/login" component={AuthContainer} />
+        <Route path="/auth/signup" component={AuthContainer} />
+      </Switch>
     </div>
   );
 }
