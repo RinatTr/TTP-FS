b4ad4901c536f08657de43347dfce2f8893be875
RinatTr <rinatreg@gmail.com>
2019-06-26 21:35:55 -0400

    validate balance

--- frontend/src/components/Portfolio/TransactionForm.js
+++ frontend/src/components/Portfolio/TransactionForm.js
@@ -9,7 +9,8 @@ export default class TransactionForm extends Component {
       symbol: "",
       amount: 0,
       isValidSym: false,
-      lastSold: 0
+      lastSold: 0,
+      isInsufficient: false
     }
   }
   handleChange = (e) => {
@@ -32,40 +33,52 @@ export default class TransactionForm extends Component {
   buyShares = () => {
     let { amount, symbol, lastSold } = this.state;
     let { loggedUser } = this.props;
-    //newTransaction
-    let bodyObj = {
-      userId: loggedUser.id,
-      symbol,
-      shares: amount,
-      value: lastSold,
-      type: "BUY",
-      newBalance: loggedUser.balance - (amount * lastSold)
-    }
+    let newBalance = loggedUser.balance - (amount * lastSold);
+    if (newBalance > 0) {
+      let bodyObj = {
+        userId: loggedUser.id,
+        symbol: symbol.toUpperCase(),
+        shares: amount,
+        value: lastSold,
+        type: "BUY",
+        newBalance
+      }
       Util.newTransaction(loggedUser.id, bodyObj)
+        .then(() => {
+          this.setState({ isInsufficient: false })
+        })
+    } else {
+      this.setState({ isInsufficient: true })
+    }
   }
 
   render() {
-    let { symbol, amount } = this.state;
+    let { symbol, amount, isInsufficient } = this.state;
+    let { loggedUser } = this.props;
     return(
-      <form onSubmit={this.handleSubmit}>
-        <input
-          type="text"
-          value={symbol}
-          name="symbol"
-          placeholder="symbol"
-          onChange={this.handleChange}
-          required
-         />
-        <input
-          type="text"
-          value={amount}
-          name="amount"
-          placeholder="amount"
-          onChange={this.handleChange}
-          required
-         />
-       <button type="submit">buy</button>
-       </form>
+      <React.Fragment>
+        <p>Your Balance: {loggedUser ? loggedUser.balance : null}</p>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={symbol}
+            name="symbol"
+            placeholder="symbol"
+            onChange={this.handleChange}
+            required
+           />
+          <input
+            type="text"
+            value={amount}
+            name="amount"
+            placeholder="amount"
+            onChange={this.handleChange}
+            required
+           />
+         <button type="submit">buy</button>
+        </form>
+        {isInsufficient ? "Insufficient Balance" : null}
+      </React.Fragment>
     )
   }
 }
